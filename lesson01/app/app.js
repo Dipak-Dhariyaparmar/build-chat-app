84b12397ce70af271a61df5880f7ea7cd2fcf8a7
Dipak-Dhariyaparmarcc <dipakdhariyaparmar@gmail.com>
2025-05-23 22:57:40 +0530

    Refactor app.js for consistent formatting and improved readability

--- lesson01/app/app.js
+++ lesson01/app/app.js
@@ -1,21 +1,20 @@
-const socket = new WebSocket('ws://localhost:3000')
+const socket = new WebSocket("ws://localhost:3000");
 
 function sendMessage(e) {
-    e.preventDefault()
-    const input = document.querySelector('input')
-    if (input.value) {
-        socket.send(input.value)
-        input.value = ""
-    }
-    input.focus()
+  e.preventDefault();
+  const input = document.querySelector("input");
+  if (input.value) {
+    socket.send(input.value);
+    input.value = "";
+  }
+  input.focus();
 }
 
-document.querySelector('form')
-    .addEventListener('submit', sendMessage)
+document.querySelector("form").addEventListener("submit", sendMessage);
 
-// Listen for messages 
+// Listen for messages from the server
 socket.addEventListener("message", ({ data }) => {
-    const li = document.createElement('li')
-    li.textContent = data
-    document.querySelector('ul').appendChild(li)
-})
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
